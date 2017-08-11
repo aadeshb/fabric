16e09b5ef50a8c0b6a4c2b5c08423f697e848a37
aadeshb <aadeshbrahm@yahoo.com>
2017-08-11 12:10:52 -0500

    Update marbles_chaincode.go

--- examples/chaincode/go/marbles02/marbles_chaincode.go
+++ examples/chaincode/go/marbles02/marbles_chaincode.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	//"bytes"
-	"encoding/json"
+	"errors"
 	"fmt"
-	//"strconv"
-	//"strings"
-	//"time"
-
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	pb "github.com/hyperledger/fabric/protos/peer"
+	"time"
+	//"strconv"
 )
 
 // SimpleChaincode example simple Chaincode implementation
@@ -25,42 +21,133 @@ type user struct {
 	Mobile     string `json:"mobile"`
 	Class	   string `json:"class"`
 }
-
-// ===================================================================================
-// Main
-// ===================================================================================
+// The main function is used to bootstrap the code, however we don't have any functionality for it right now
+// it only reports if an error occurs, which never should
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
+
+
 }
 
-// Init initializes chaincode
-// ===========================
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	return shim.Success(nil)
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	//If we are given more than one argument for the init function, then it errors
+	//Init shouldn't need any arguments at all actually
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+	//We are writing some key under the name "hello_world" to the ledger, the hello world is simply a key to hold whatever
+	//argument we pass, if we query "hello_world" with the query function, then it would return whatever argument is placed in args[0]
+	err := stub.PutState("hello_world", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
 
-// Invoke - Our entry point for Invocations
-// ========================================
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	function, args := stub.GetFunctionAndParameters()
+// Invoke is the entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
-	if function == "initUser" { //create a new user
+	//If the init function is called, then we send the args to the init command to be stored under the "hello_world" key
+	if function == "init" {
+		return t.Init(stub, "init", args)
+	} else if function == "Register" {
+		return t.Register(stub, args)
+	} else if function == "makePurchaseOrder" {
+		return t.makePurchaseOrder(stub, args)
+	} else if function == "initUser"{
 		return t.initUser(stub, args)
-	} 
+	}
 
-	fmt.Println("invoke did not find func: " + function) //error
-	return shim.Error("Received unknown function invocation")
+	fmt.Println("invoke did not find func: " + function)
+
+	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-// ============================================================
-// inituser - create a new user, store into chaincode state
-// ============================================================
-func (t *SimpleChaincode) initUser(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	// If the read function is called the read function activiates
+	if function == "read" { //read a variable
+		return t.read(stub, args)
+	}
+	fmt.Println("query did not find func: " + function)
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
+
+//=============================================================================================================================================
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) Register(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename 
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+
+// Purchase order code and "write" code are the exact same, because in essence, both should do the same job, which is to write
+// data to the ledger which can be read later on 
+// makePurchaseOrder has two user given inputs, 1 - supplier id, 2- manufacturer id
+func (t *SimpleChaincode) makePurchaseOrder(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
+
+	var key, value string
+	var err error
+	
+	var a = time.Now()
+	var b = a.Format("20060102150405") 
+	key = args[0] //the key is simply the suppliers id
+	var body = args[2]
+	value = args[1] + "-" + b +"-"+  key + " " + body
+	//var comm string = value + b + key
+	
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+func (t *SimpleChaincode) initUser(stub shim.ChaincodeStubInterface, args []string) ([]byte, error)  {
 	var err error
 
 	//   	0       	1         2       3        4		 5
@@ -76,12 +163,12 @@ func (t *SimpleChaincode) initUser(stub shim.ChaincodeStubInterface, args []stri
 
 	// ==== Check if user already exists ====
 	userAsBytes, err := stub.GetState(mobile)
-	if err != nil {
-		return shim.Error("Failed to get user: " + err.Error())
-	} else if userAsBytes != nil {
-		fmt.Println("This user already exists: " + firstName)
-		return shim.Error("This user already exists: " + firstName)
-	}
+	//if err != nil {
+//		return shim.Error("Failed to get user: " + err.Error())
+//	} else if userAsBytes != nil {
+//		fmt.Println("This user already exists: " + firstName)
+//		return shim.Error("This user already exists: " + firstName)
+//	}
 
 	// ==== Create user object and marshal to JSON ====
 	objectType := "user"
@@ -119,3 +206,4 @@ func (t *SimpleChaincode) initUser(stub shim.ChaincodeStubInterface, args []stri
 	fmt.Println("- end init user")
 	return shim.Success(nil)
 }
+
